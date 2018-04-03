11ba93f704853601128bda882182fa3e14062480
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-04-04 00:45:43 +0600

    Update 3.1.Swap_two_number.c
    Enter value of x y and z
    1
    2
    3
    Before swapping
     x=1
     y=2
     z=3
    After swapping
     x=2
     y=3
     z=1

--- 3.Chapter 3/Programming Exercise/3.1.Swap_two_number.c
+++ 3.Chapter 3/Programming Exercise/3.1.Swap_two_number.c
@@ -15,3 +15,23 @@ int main()
     return 0;
 
 }
+###Swap 3 number###
+
+#include<stdio.h>
+int main()
+{
+    int x,y,z,temp;
+    printf("Enter value of x y and z\n");
+    scanf("%d %d %d",&x,&y,&z);
+
+    printf("Before swapping\n x=%d\n y=%d\n z=%d\n",x,y,z);
+
+    temp=x;
+    x=y;
+    y=z;
+    z=temp;
+
+    printf("After swapping\n x=%d\n y=%d\n z=%d",x,y,z);
+    return 0;
+
+}
