910004840196242cb2080fe172f85a1699343f1b
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-04-19 22:34:17 +0600

    Create Grade_sheet.c

---
+++ Grade_sheet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+	int number,index;
+	char grade;
+
+ 	printf("Enter Marks");
+    scanf(" %d",&number);
+    index=number/10;
+	switch(index)
+	{
+		case 10:
+			grade='A';
+			break;
+		case 9:
+			grade='B';
+			break;
+		case '8':
+			grade='C';
+			break;
+		case 7:
+		case 6:
+			grade='D';
+			break;
+		default:
+			printf("Invalid Operation\n");
+			break;
+	}
+	printf("Grade is=%s",grade);
+	return 0;
+}
