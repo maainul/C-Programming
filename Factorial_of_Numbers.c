ec047899a7b975d6a55b70a806fcb8c873caeb1e
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-04-19 21:34:56 +0600

    Update and rename Factorial of Numbers to Factorial_of_Numbers.c

--- Factorial_of_Numbers.c
+++ Factorial_of_Numbers.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
- 
 int main()
 {
   int c, n, fact = 1;
@@ -8,9 +7,7 @@ int main()
   scanf("%d", &n);
  
   for (c = 1; c <= n; c++)
-    fact = fact * c;
- 
+    fact = fact * c; 
   printf("Factorial of %d = %d\n", n, fact);
- 
   return 0;
 }
