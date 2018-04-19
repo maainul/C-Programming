2b7ceb1f8c1c0e5a0cb23274c1b44e3dab5600c4
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-04-19 21:37:56 +0600

    Update and rename Sum of numbers to Sum_of_numbers.c

--- Sum_of_numbers.c
+++ Sum_of_numbers.c
@@ -1,22 +1,16 @@
-#include <stdio.h>
- 
+#include <stdio.h> 
 int main()
 {
   int n, sum = 0, c, value;
- 
   printf("How many numbers you want to add?\n");
   scanf("%d", &n);
- 
   printf("Enter %d integers\n", n);
- 
   for (c = 1; c <= n; c++)
   {
     scanf("%d", &value);
     sum = sum + value;
   }
- 
   printf("Sum of the integers = %d\n", sum);
- 
   return 0;
 }
 
