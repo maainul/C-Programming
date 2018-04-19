33bbfc6ce6583e6c6a078bf9c5bc3ed727f1b32e
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-04-19 21:28:31 +0600

    Update and rename 16.palindrome_number_program to 16.palindrome_number_program.c

--- 16.palindrome_number_program.c
+++ 16.palindrome_number_program.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
- 
 int main()
 {
    int n, reverse = 0, t;
- 
    printf("Enter a number to check if it is a palindrome or not\n");
    scanf("%d", &n);
- 
    t = n;
- 
    while (t != 0)
    {
       reverse = reverse * 10;
