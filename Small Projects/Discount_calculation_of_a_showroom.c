eec1842fd741ae54e6b714d853cd7d36d8989590
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-04-20 10:36:37 +0600

    Create Discount_calculation_of_a_showroom.c

---
+++ Small Projects/Discount_calculation_of_a_showroom.c
@@ -0,0 +1,64 @@
+/*.............marketing of a showroom..................*/
+#define MC1 0
+#define MC2 0.05
+#define MC3 0.075
+#define MC4 0.10
+#define HI1 0.05
+#define HI2 0.075
+#define HI3 0.10
+#define HI4 0.15
+#include<stdio.h>
+int main ()
+{
+	//variable decleration
+	int level,customerNumber;
+	float price,basic,net,discount;
+	//Input value
+	input:
+	printf("Enter level, customerNumber and Price\n");
+	printf("Enter 0 for level to END\n");
+	scanf("%d",&level);
+
+	//check the level number of the products
+	if(level==0) goto stop;
+	scanf("%d %f",&customerNumber,&price);
+
+	switch(level)
+	{
+		case 1:
+			discount=MC1+HI1;
+			break;
+		case 2:
+			discount=MC2+HI2;
+			break;
+		case 3:
+			discount=MC3+HI3;
+			break;
+		case 4:
+			discount=MC4+HI4;
+			break;
+		default:
+			printf("Error in Level code\n");
+			goto stop;
+
+	}
+
+	if(0<=price<=100)
+		net=price-(price*discount);
+
+	else if(101<=price<=200)
+		net=price-(price*discount);
+
+else if(201<=price<=300)
+		net=price-(price*discount);
+
+	else
+		net=price-(price*discount);
+
+
+		printf("Net amount=%f\n",net);
+	goto input;
+
+	stop:
+		printf("End of the program\n");
+}
