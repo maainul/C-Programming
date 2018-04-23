c4a22236e9d41cd13afb591bb6331dada26c9a29
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-04-23 21:24:19 +0600

    addition of  2*3 array

---
+++ 2D_array_addition.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,num[2][3],total=0;
+    printf("Enter 6 Values:\n");
+    /*.....Reading and Printing Value......*/
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&num[i][j]);
+        }
+    }
+    /*....Printing Value......*/
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("\t%d",num[i][j]);
+            total=total+num[i][j];
+        }
+        printf("\t%d",total);
+        total=0;
+        printf("\n");
+    }
+    return 0;
+}
