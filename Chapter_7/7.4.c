caa701a68cfab0a4b4f590788b7e42cf04086f86
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-04-24 08:37:25 +0600

    Update 7.4

--- Chapter_7/7.4.c
+++ Chapter_7/7.4.c
@@ -1,39 +1,59 @@
-#include<stdio.h>
-int main()
+#define MAXGIRLS 4
+#define MAXTERM 3
+#include <stdio.h>
+int main ()
 {
-    //number[row][column]
-    int i,j,num[4][3],total=0;
-    printf("Enter 6 Values:\n");
-    /*.....Reading and Printing Value......*/
-    for(i=0;i<4;i++)//Number of Rows
+    int value[MAXGIRLS][MAXTERM];
+    int i,j,grand_total;
+    int girl_total[MAXGIRLS],item_total[MAXTERM];
+    /*..........READING OF VALUES AND COMPUTING GIRL_TOTAL...............*/
+    printf("Input Data\n");
+    printf("Enter Values, one at a time,row-wise\n\n");
+    /*..........COMPUTE GIRLS TOTALS Row Value Addition...............*/
+    for(i=0;i<MAXGIRLS;i++)
     {
-        printf("Enter %d Rows:\n",i+1);
-
-        for(j=0;j<3;j++)
+        printf("Input %d Row\n",i+1);
+        girl_total[i]=0;
+        for(j=0;j<MAXTERM;j++)
         {
-
-            scanf("%d",&num[i][j]);
+            scanf("%d",&value[i][j]);
+            girl_total[i]=girl_total[i]+value[i][j];
         }
     }
-    /*....Printing Value......*/
-    for(i=0;i<4;i++)
+    /*......print Matrix Line....*/
+     for(i=0;i<MAXGIRLS;i++)
     {
-        for(j=0;j<3;j++)
+        for(j=0;j<MAXTERM;j++)
         {
-            printf("\t%d",num[i][j]);
+            printf("\t%d",value[i][j]);
         }
         printf("\n");
     }
-    //Printing Total Amount
-    for(i=0;i<4;i++)
+    /*...........COMPUTE ITEMS TOTALS (Column Value ADDINTION)..............*/
+    for(j=0;j<MAXTERM;j++)
     {
-        for(j=0;j<3;j++)
+        item_total[j]=0;
+        for(i=0;i<MAXGIRLS;i++)
         {
-            total=total+num[i][j];
+            item_total[j]=item_total[j]+value[i][j];
         }
-        printf("Salesgirl[%d]=%d",i+1,total);
-        total=0;
-        printf("\n");
     }
-    return 0;
+    /*.........COMPUTING GRAND TOTALS................*/
+    grand_total=0;
+    for(i=0;i<MAXGIRLS;i++)
+        grand_total=grand_total+girl_total[i];
+    /*.......PRINTING RESULT......*/
+    printf("\n Girls TOTALS\n");
+    /*.....GIRLS TOTALS.......*/
+    for(i=0;i<MAXGIRLS;i++)
+    {
+        printf("SalesGirl[%d]=%d\n",i+1,girl_total[i]);
+    }
+      /*.....ITEMS TOTALS.......*/
+    printf("\n ITEMS TOTALS\n");
+    for(j=0;j<MAXTERM;j++)
+        printf("ITEM[%d]=%d\n",j+1,item_total[j]);
+        printf("GRAND TOTAL=%d\n",grand_total);
+
+return 0;
 }
