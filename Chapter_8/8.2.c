e77fa6dde12311840d822489dda1a5de3ce5823a
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-04-29 23:53:33 +0600

    Create 8.2
    Write a program to read a line of text containing a series of words from the terminal

---
+++ Chapter_8/8.2.c
@@ -0,0 +1,22 @@
+/*...
+Write a program to read a line of text containing a series of words from the terminal
+..*/
+#include<stdio.h>
+int main ()
+{
+    char line[81],character;
+    int c;
+    c=0;
+    printf("Enter Text. Press <Return> at end\n");
+    do{
+        character=getchar();
+        line[c]=character;
+        c++;
+    }
+    while(character!='\n');
+    c=c-1;
+    line[c]='\0';
+    printf("n%s\n",line);
+    return 0;
+}
+
