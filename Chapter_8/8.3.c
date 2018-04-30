2979ebb3659979ab30e5e3e710ac1ac00f4be428
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-04-30 09:02:37 +0600

    Create 8.3

---
+++ Chapter_8/8.3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char string1[80],string2[80];
+    int i;
+    printf("Enter a string:\n");
+    printf("?");
+    scanf("%s",string2);
+    for(i=0;string2[i] !='\0';i++)
+        string1[i]=string2[i];
+    string1[i]='\0';
+    printf("\n");
+    printf("%s\n",string1);
+    printf("Number of Characters =%d\n",i);
+    return 0;
+}
