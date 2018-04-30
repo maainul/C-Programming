88ec6d5b1993b68358935de05098cee6a3523a98
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-04-30 09:53:32 +0600

    Update 8.6

--- Chapter_8/8.6.c
+++ Chapter_8/8.6.c
@@ -1,6 +1,4 @@
-
-
----------------
+--------------
 |CProgramming|
 |CProgramming|
 |CProgramming|
@@ -12,7 +10,7 @@
 |CProgramming|
 |CProgramming|
 |CProgramming|
-----------------
+--------------
 |CProgramming|
 |CProgramming|
 |CProgramming|
@@ -23,7 +21,7 @@
 |CProgramming|
 |CProgramming|
 |CProgramming|
-------------------
+--------------
 #include <stdio.h>
 int main()
 {
@@ -43,3 +41,181 @@ int main()
 	}
 	printf("------------------\n");
 }
+
+--------------
+|C           |
+|CP          |
+|CPr         |
+|CPro        |
+|CProg       |
+|CProgr      |
+|CProgra     |
+|CProgram    |
+|CProgramm   |
+|CProgrammi  |
+|CProgrammin |
+|CProgrammi  |
+|CProgramm   |
+|CProgram    |
+|CProgra     |
+|CProgr      |
+|CProg       |
+|CPro        |
+|CPr         |
+|CP          |
+|C           |
+--------------
+#include <stdio.h>
+int main()
+{
+	int c,d;
+	char string[]="CProgramming";
+	printf("\n\n");
+	printf("---------------\n");
+	for (int c = 0; c < 11; ++c)
+	{
+		d=c+1;
+		printf("|%-12.*s|\n",d,string);
+	}
+	for (int c = 11; c> 1; --c)
+	{
+	    d=c-1;
+		printf("|%-12.*s|\n",d,string);
+	}
+	printf("------------------\n");
+}
+
+--------------
+|           C|
+|          CP|
+|         CPr|
+|        CPro|
+|       CProg|
+|      CProgr|
+|     CProgra|
+|    CProgram|
+|   CProgramm|
+|  CProgrammi|
+| CProgrammin|
+|  CProgrammi|
+|   CProgramm|
+|    CProgram|
+|     CProgra|
+|      CProgr|
+|       CProg|
+|        CPro|
+|         CPr|
+|          CP|
+|           C|
+--------------
+
+
+#include <stdio.h>
+int main()
+{
+	int c,d;
+	char string[]="CProgramming";
+	printf("\n\n");
+	printf("---------------\n");
+	for (int c = 0; c < 11; ++c)
+	{
+		d=c+1;
+		printf("|%12.*s|\n",d,string);
+	}
+	for (int c = 11; c> 1; --c)
+	{
+	    d=c-1;
+		printf("|%12.*s|\n",d,string);
+	}
+	printf("------------------\n");
+}
+
+
+--------------
+|C|
+|CP|
+|CPr|
+|CPro|
+|CProg|
+|CProgr|
+|CProgra|
+|CProgram|
+|CProgramm|
+|CProgrammi|
+|CProgrammin|
+|CProgrammi|
+|CProgramm|
+|CProgram|
+|CProgra|
+|CProgr|
+|CProg|
+|CPro|
+|CPr|
+|CP|
+|C|
+--------------
+#include <stdio.h>
+int main()
+{
+	int c,d;
+	char string[]="CProgramming";
+	printf("\n\n");
+	printf("---------------\n");
+	for (int c = 0; c < 11; ++c)
+	{
+		d=c+1;
+		printf("|%.*s|\n",d,string);
+	}
+	for (int c = 11; c> 1; --c)
+	{
+	    d=c-1;
+		printf("|%.*s|\n",d,string);
+	}
+	printf("------------------\n");
+}
+
+
+---------------
+|C|
+| C|
+|  C|
+|   C|
+|    C|
+|     C|
+|      C|
+|       C|
+|        C|
+|         C|
+|          C|
+|         C|
+|        C|
+|       C|
+|      C|
+|     C|
+|    C|
+|   C|
+|  C|
+| C|
+|C|
+------------------
+	
+#include <stdio.h>
+int main()
+{
+	int c,d;
+	char string[]="CProgramming";
+	printf("\n\n");
+	printf("---------------\n");
+	for (int c = 0; c < 11; ++c)
+	{
+		d=c+1;
+		printf("|%*.1s|\n",d,string);
+	}
+	for (int c = 11; c> 1; --c)
+	{
+	    d=c-1;
+		printf("|%*.1s|\n",d,string);
+	}
+	printf("------------------\n");
+}
+
