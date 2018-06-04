905aec5b33954b48d8351f320a5c47a3383e5af8
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-06-05 04:04:52 +0600

    Update Basic_pointer.c

--- Chapter_11/Basic_pointer.c
+++ Chapter_11/Basic_pointer.c
@@ -4,9 +4,9 @@ int main()
 	int a=100;
 	int *p;
 	p=&a;
-	printf("Value of A:\n",a);
-	printf("Address of A:\n",*p);
+	printf("Value of A:%d\n",a);
+	printf("Address of A:%d\n",*p);
+	printf("%p\n",&a);
+	printf("%p",&p);
 	return 0;
-
-
 }
