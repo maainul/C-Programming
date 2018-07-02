5012043a2b0fabb5b806df513d1a4887c48ea534
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-07-03 00:17:36 +0600

    Create Biggest_number_of_an_array.c

---
+++ Shahin_sir/Biggest_number_of_an_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+	int i,j,A[100],Elements,Biggest=0;
+	printf("Enter number of Elements:\n");
+	scanf("%d",&Elements);
+	printf("Elements are:\n");
+	for (i = 0; i < Elements; ++i)
+	{
+		scanf("%d",&A[i]);
+	}
+	//Print the result
+	for (i = 0; i < Elements; ++i)
+	{
+		printf("%d\t",A[i]);
+	}
+
+	//Biggest Elements
+	for (int i = 0; i <Elements; ++i)
+	{
+		if (A[i]>Biggest)
+		{
+			Biggest=A[i];
+		}
+	}
+	printf("\nBiggest number is:%d\n",Biggest);
+	return 0;
+}
