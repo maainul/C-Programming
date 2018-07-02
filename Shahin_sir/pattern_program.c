c96720263bb664f94d202c26118ce5af7221e382
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-07-02 16:17:48 +0600

    Update pattern_program.c

--- Shahin_sir/pattern_program.c
+++ Shahin_sir/pattern_program.c
@@ -1,10 +1,12 @@
+--------------------------------
+Program :1
 OUTPUT:
 *
 **
 ***
 ****
 *****
---------------------------------
+
 #include<stdio.h>
 int main()
 {
@@ -19,4 +21,153 @@ int main()
     }
     return 0;
 }
+
+        End of First Program
 ---------------------------------
+ 
+--------------------------------
+Program 2:
+OUTPUT:
+*****
+****
+***
+**
+*
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=5;j>i;j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+        End Of Second Program
+---------------------------------
+
+
+----------------------------------
+Program 3:
+OUTPUT:
+*****
+ ****
+  ***
+   **
+    *
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            if(j<i)
+            printf(" ");
+            else
+                printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+        End of third Program 
+-----------------------------------
+
+-----------------------------------
+Program 4:
+OUTPUT: 
+    *
+   **
+  ***
+ ****
+*****
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=4;j>=0;j--)
+        {
+            if(j>i)
+            printf(" ");
+            else
+                printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+        End of fourth Program 
+---------------------------------
+
+---------------------------------
+Program 5:
+OUTPUT:
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=4;j>=0;j--)
+        {
+            if(j>i)
+            printf(" ");
+            else
+                printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+        End of fifth Program
+---------------------------------
+
+
+-----------------------------------
+Extra Program:
+
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            if(j<i)
+            printf(" ");
+            else
+                printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+-----------------------------------
+
