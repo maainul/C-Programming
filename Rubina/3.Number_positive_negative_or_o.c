13e8fe3fe6945e403b7de7dd5f1458ce91712a5b
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-07-03 01:29:51 +0600

    Create 3.Number_positive_negative_or_o.c

---
+++ Rubina/3.Number_positive_negative_or_o.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	int num;
+	printf("Enter a number\n");
+	scanf("%d",&num);
+	if(num>0)
+	{
+		printf("Number is positive");
+	}
+	else if (num==0)
+	{
+        printf("0");
+	}
+	else
+	{
+		printf("Number is negative");
+	}
+
+
+	return 0;
+}
