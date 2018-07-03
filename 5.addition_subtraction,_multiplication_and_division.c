780029127d8c4f2b97819834adc455ef327d91a8
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-07-03 18:35:41 +0600

    Create 5.addition_subtraction,_multiplication_and_division.c

---
+++ 5.addition_subtraction,_multiplication_and_division.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter integer number:\n");
+    scanf("%d%d",&a,&b);
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+    return 0;
+}
+
+
