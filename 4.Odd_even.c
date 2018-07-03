e2a4f44ba4c25a4e7824fa8b1ecf5f9759718728
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-07-03 18:32:00 +0600

    Update 4.Odd_even.c

--- 4.Odd_even.c
+++ 4.Odd_even.c
@@ -16,3 +16,20 @@
      
     return 0;
 }
+
+## USING IF ELSE
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter integer number:\n");
+    scanf("%d",&a);
+    if(a%2==0)
+    printf("Even Number");
+    else
+    printf("Odd");
+    return 0;
+}
+
+
