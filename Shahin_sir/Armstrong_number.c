50e177ac4758aa4f77fc1e8927871101236dc75b
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-07-04 01:29:54 +0600

    Create Armstrong_number.c

---
+++ Shahin_sir/Armstrong_number.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int number, originalNumber, remainder, result = 0;
+
+    printf("Enter a three digit integer: ");
+    scanf("%d", &number);
+
+    originalNumber = number;
+
+    while (originalNumber != 0)
+    {
+        remainder = originalNumber%10;
+        result += remainder*remainder*remainder;
+        originalNumber /= 10;
+    }
+
+    if(result == number)
+        printf("%d is an Armstrong number.",number);
+    else
+        printf("%d is not an Armstrong number.",number);
+
+    return 0;
+}
+/*
+Armstrong number 1: 0
+Armstrong number 2: 1
+Armstrong number 3: 153
+Armstrong number 4: 370
+Armstrong number 5: 371
+Armstrong number 6: 407
+*/
