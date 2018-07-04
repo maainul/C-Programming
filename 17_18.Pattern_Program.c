836a9113653053f75e696aa67ccd6273e21be4b0
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-07-04 10:08:43 +0600

    Update and rename 18.Pattern_Program.c to 17_18.Pattern_Program.c

--- 17_18.Pattern_Program.c
+++ 17_18.Pattern_Program.c
@@ -468,4 +468,60 @@ int main ()
     return 0;
 }
 
+OUTPUT:
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=4;j>=0;j--)
+        {
+            if(j>i)
+            printf(" ");
+            else
+                printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+        End of fifth Program
+---------------------------------
+
+
+-----------------------------------
+Extra Program:
+
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            if(j<i)
+            printf(" ");
+            else
+                printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
 
