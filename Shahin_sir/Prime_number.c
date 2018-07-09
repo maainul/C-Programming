d9afce708224d7e66cbe685f34f0b1f318fdd0e6
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-07-09 16:53:14 +0600

    Update Prime_number.c

--- Shahin_sir/Prime_number.c
+++ Shahin_sir/Prime_number.c
@@ -4,18 +4,19 @@ main() {
   int n, i, c = 0;
   printf("Enter any number n:");
   scanf("%d", &n);
-
-  /*logic*/  for (i = 1; i <= n; i++) {
+  /*logic*/  
+  for (i = 2; i < n/2; i++) {
       if (n % i == 0) {
-         c++;
+         c=1;
+        break;
       }
   }
-
-  if (c == 2) {
-  printf("%d is a Prime number",n);
+/*Condition Check*/
+  if (c == 1) {
+  printf("%d is Not Prime number",n);
   }
   else {
-  printf("%d is not a Prime number",n);
+  printf("%d is a Prime number",n);
   }
   return 0;
 }
