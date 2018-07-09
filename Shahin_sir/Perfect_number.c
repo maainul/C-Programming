254fd2137c38c94043a32561baa070e58ed0cb85
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2018-07-10 00:12:58 +0600

    Update Perfect_number.c

--- Shahin_sir/Perfect_number.c
+++ Shahin_sir/Perfect_number.c
@@ -1,5 +1,11 @@
 /*
  * C Program to Check whether a given Number is Perfect Number
+ Perfect number, a positive integer that is equal to the sum of
+ its proper divisors. The smallest perfect number is 6, which is the sum of 1, 2, and 3.
+ 15 is divisible by 1,3,5
+ so =1+3+5=9
+ if number is equal to sum of its divisor is called perfect number
+ here sum of divisor is not the given number so it is not perfect number.
  */
 #include <stdio.h>
  
